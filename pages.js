f5fdf1550a197c507bcd60ed83db08f270a5475c
Ikonde Nekemiah Arnold <i-nad@localhost.localdomain>
2023-10-30 10:04:37 +0300

    fixed github pages

--- pages.js
+++ pages.js
@@ -1,16 +1,15 @@
 const ghpages = require("gh-pages");
-const pathname = `${__dirname}/build`;
+const path = require("path");
 const repoURL = "https://github.com/arnoldnekemiah/portfolio-final.git";
 
-
 ghpages.publish(
-  pathname,
+  path.join(__dirname, "build"), // Use path.join to ensure the correct build path
   {
-    branch: "master",
+    branch: "gh-pages", // Use "gh-pages" branch for GitHub Pages
     repo: repoURL,
   },
   (err) => {
-    if (err) console.log("ERROR: ", err);
+    if (err) console.error("ERROR: ", err);
     else console.log("PUBLISHED");
   }
 );
